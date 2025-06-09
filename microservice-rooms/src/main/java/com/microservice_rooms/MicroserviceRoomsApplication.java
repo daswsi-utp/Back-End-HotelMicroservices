af56f69590faae60d1773fdc1bcbb73662c7573d
Im3Im3 <manuelcode737@gmail.com>
2025-06-09 11:10:51 -0500

    Edit Microservice Rooms

---
+++ microservice-rooms/src/main/java/com/microservice_rooms/MicroserviceRoomsApplication.java
@@ -0,0 +1,13 @@
+package com.microservice_rooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceRoomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceRoomsApplication.class, args);
+	}
+
+}
