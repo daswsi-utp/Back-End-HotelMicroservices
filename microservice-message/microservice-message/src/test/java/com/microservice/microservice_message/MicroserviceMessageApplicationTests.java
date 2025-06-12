ddcdfed0f57b7466176c875a0c7c1daa1c7ec6e9
R0dn3 <u19305755@utp.edu.pe>
2025-06-12 16:02:32 -0500

    microservice-message file creation
    # Conflicts:
    #	.idea/compiler.xml
    #	.idea/encodings.xml
    #	.idea/material_theme_project_new.xml
    #	microservice-promotions/.mvn/wrapper/maven-wrapper.properties
    #	microservice-promotions/pom.xml
    #	microservice-rooms/src/main/resources/application.yml
    #	pom.xml

---
+++ microservice-message/microservice-message/src/test/java/com/microservice/microservice_message/MicroserviceMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.microservice_message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceMessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ microservice-promotions/src/test/java/com/microservice/promotions/microservice_promotions/MicroservicePromotionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.promotions.microservice_promotions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicePromotionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
