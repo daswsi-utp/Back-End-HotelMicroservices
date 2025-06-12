9cb31b4b6938d6aa76abcfdcc69661870ffe86d2
R0dn3 <u19305755@utp.edu.pe>
2025-06-12 16:10:19 -0500

    add config folder - CacheMessageConfig

---
+++ microservice-message/src/main/java/com/microservice/config/CacheMessageConfig.java
@@ -0,0 +1,10 @@
+package com.microservice.microservice_message.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheMessageConfig {
+
+}
