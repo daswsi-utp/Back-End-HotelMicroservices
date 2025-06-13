5b87b03fcbd18e81f7e62a9245ffc113e7894658
KenyiPlatanazo <riositalo2@gmail.com>
2025-06-13 16:03:43 -0500

    REFACTOR: Minor changes across promotions finally enable Gateway connection
    Changes will be tried in the original branch for good measure

--- microservice-promotions/src/main/java/com/microservice_promotions/MicroservicePromotionsApplication.java
+++ microservice-promotions/src/main/java/com/microservice_promotions/MicroservicePromotionsApplication.java
@@ -2,7 +2,9 @@ package com.microservice_promotions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MicroservicePromotionsApplication {
 
--- microservice-promotions/src/main/java/com/microservice_promotions/controller/PromotionController.java
+++ microservice-promotions/src/main/java/com/microservice_promotions/controller/PromotionController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/promotions")
+@RequestMapping
 public class PromotionController {
     @Autowired
     private IPromotionService iPromotionService;
