f118fca684cc435460a144063d2c9519614e48b9
italo <riositalo2@gmail.com>
2025-06-13 17:57:50 -0500

    ADDED: Unit test for saving process of promotion

--- microservice-promotions/src/test/java/com/microservice_promotions/MicroservicePromotionsApplicationTests.java
+++ microservice-promotions/src/test/java/com/microservice_promotions/MicroservicePromotionsApplicationTests.java
@@ -1,10 +1,35 @@
 package com.microservice_promotions;
 
+import com.microservice_promotions.entitites.Promotion;
+import com.microservice_promotions.service.IPromotionService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Date;
 
 @SpringBootTest
 class MicroservicePromotionsApplicationTests {
+	@Autowired
+	private IPromotionService iPromotionService;
+
+	@Test
+	public void Promotion_Save_ReturnSavedPromotion(){
+		Promotion promotion = Promotion.builder().
+				name("Test")
+				.description("Test description")
+				.discountValue(20.0)
+				.startDate(Date.valueOf("2025-06-15"))
+				.endDate(Date.valueOf( "2025-07-15"))
+				.type(Promotion.Type.percentage)
+				.build();
+		Promotion savedPromotion = iPromotionService.save(promotion);
+		Assertions.assertThat(savedPromotion).isNotNull();
+		Assertions.assertThat(savedPromotion.getCreatedAt()).isNotNull();
+		Assertions.assertThat(savedPromotion.getUpdatedAt()).isNotNull();
+		Assertions.assertThat(savedPromotion.getPromotionId()).isGreaterThan(0);
+	}
 
 	@Test
 	void contextLoads() {
