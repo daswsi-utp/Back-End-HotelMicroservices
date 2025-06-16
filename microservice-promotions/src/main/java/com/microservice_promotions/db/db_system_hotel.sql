300023a5258be389412096a7e5e64fe4edd7af89
italo <riositalo2@gmail.com>
2025-06-16 08:28:07 -0500

    UPDATED: Promotions table to support enum applicability field

--- microservice-promotions/src/main/java/com/microservice_promotions/db/db_system_hotel.sql
+++ microservice-promotions/src/main/java/com/microservice_promotions/db/db_system_hotel.sql
@@ -2,15 +2,14 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 17.4
--- Dumped by pg_dump version 17.4
+-- Dumped from database version 16.8
+-- Dumped by pg_dump version 16.8
 
--- Started on 2025-06-15 22:29:10
+-- Started on 2025-06-16 08:26:49 -05
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
-SET transaction_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
@@ -20,7 +19,20 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 849 (class 1247 OID 25051)
+-- TOC entry 849 (class 1247 OID 25102)
+-- Name: applicability; Type: TYPE; Schema: public; Owner: postgres
+--
+
+CREATE TYPE public.applicability AS ENUM (
+    'all',
+    'selected'
+);
+
+
+ALTER TYPE public.applicability OWNER TO postgres;
+
+--
+-- TOC entry 843 (class 1247 OID 25075)
 -- Name: promotion_type; Type: TYPE; Schema: public; Owner: postgres
 --
 
@@ -34,7 +46,15 @@ CREATE TYPE public.promotion_type AS ENUM (
 ALTER TYPE public.promotion_type OWNER TO postgres;
 
 --
--- TOC entry 4567 (class 2605 OID 25057)
+-- TOC entry 4204 (class 2605 OID 25108)
+-- Name: CAST (character varying AS public.applicability); Type: CAST; Schema: -; Owner: -
+--
+
+CREATE CAST (character varying AS public.applicability) WITH INOUT AS IMPLICIT;
+
+
+--
+-- TOC entry 4203 (class 2605 OID 25081)
 -- Name: CAST (character varying AS public.promotion_type); Type: CAST; Schema: -; Owner: -
 --
 
@@ -42,7 +62,7 @@ CREATE CAST (character varying AS public.promotion_type) WITH INOUT AS IMPLICIT;
 
 
 --
--- TOC entry 219 (class 1255 OID 25058)
+-- TOC entry 217 (class 1255 OID 25082)
 -- Name: delete_test_entries(); Type: FUNCTION; Schema: public; Owner: postgres
 --
 
@@ -65,7 +85,7 @@ $$;
 ALTER FUNCTION public.delete_test_entries() OWNER TO postgres;
 
 --
--- TOC entry 220 (class 1255 OID 25059)
+-- TOC entry 218 (class 1255 OID 25083)
 -- Name: update_updated_at_column(); Type: FUNCTION; Schema: public; Owner: postgres
 --
 
@@ -86,7 +106,7 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 217 (class 1259 OID 25060)
+-- TOC entry 215 (class 1259 OID 25084)
 -- Name: promotions; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -102,6 +122,7 @@ CREATE TABLE public.promotions (
     type public.promotion_type DEFAULT 'percentage'::public.promotion_type NOT NULL,
     is_active boolean DEFAULT true NOT NULL,
     minimun_stay bigint DEFAULT 1,
+    room_applicability public.applicability DEFAULT 'all'::public.applicability,
     CONSTRAINT check_discount_value CHECK ((((type = 'percentage'::public.promotion_type) AND (discount_value >= ((0)::numeric)::double precision) AND (discount_value <= ((100)::numeric)::double precision)) OR ((type = 'fixed'::public.promotion_type) AND (discount_value >= ((0)::numeric)::double precision)) OR ((type = 'added_value'::public.promotion_type) AND (discount_value IS NULL))))
 );
 
@@ -109,7 +130,7 @@ CREATE TABLE public.promotions (
 ALTER TABLE public.promotions OWNER TO postgres;
 
 --
--- TOC entry 218 (class 1259 OID 25070)
+-- TOC entry 216 (class 1259 OID 25095)
 -- Name: promotions_promotion_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -125,8 +146,8 @@ CREATE SEQUENCE public.promotions_promotion_id_seq
 ALTER SEQUENCE public.promotions_promotion_id_seq OWNER TO postgres;
 
 --
--- TOC entry 4810 (class 0 OID 0)
--- Dependencies: 218
+-- TOC entry 4446 (class 0 OID 0)
+-- Dependencies: 216
 -- Name: promotions_promotion_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -134,7 +155,7 @@ ALTER SEQUENCE public.promotions_promotion_id_seq OWNED BY public.promotions.pro
 
 
 --
--- TOC entry 4647 (class 2604 OID 25071)
+-- TOC entry 4284 (class 2604 OID 25096)
 -- Name: promotions promotion_id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -142,25 +163,25 @@ ALTER TABLE ONLY public.promotions ALTER COLUMN promotion_id SET DEFAULT nextval
 
 
 --
--- TOC entry 4803 (class 0 OID 25060)
--- Dependencies: 217
+-- TOC entry 4439 (class 0 OID 25084)
+-- Dependencies: 215
 -- Data for Name: promotions; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.promotions (promotion_id, promotion_name, promotion_description, discount_value, start_date, end_date, created_at, updated_at, type, is_active, minimun_stay) FROM stdin;
-2	Summer Sale	20% off all summer items	30	2025-06-14	2025-07-15	\N	\N	percentage	f	1
-3	Summer Sale	20% off all summer items	30	2025-06-14	2025-07-15	\N	\N	percentage	f	1
-4	Summer Sale	20% off all summer items	40	2025-06-14	2025-07-15	\N	\N	percentage	f	1
-1	Summer Sale	20% off all summer items	20	2025-06-14	2025-07-15	\N	2025-06-11 18:30:16.060139	percentage	f	1
-5	Spooky Sale	20% off all hallowing items	20	2025-06-14	2025-07-15	2025-06-11 18:34:47.58427	2025-06-11 18:34:47.58427	percentage	f	1
-7	Spooky Sale Added Value	20% off all hallowing items + Free Wine	\N	2025-06-14	2025-07-15	2025-06-11 18:42:04.111194	2025-06-11 18:42:04.111194	added_value	f	1
-14	Regular	Test description	20	2025-06-15	2025-07-15	2025-06-13 19:16:41.736387	2025-06-13 19:16:41.736387	percentage	t	1
+COPY public.promotions (promotion_id, promotion_name, promotion_description, discount_value, start_date, end_date, created_at, updated_at, type, is_active, minimun_stay, room_applicability) FROM stdin;
+2	Summer Sale	20% off all summer items	30	2025-06-14	2025-07-15	\N	\N	percentage	f	1	all
+3	Summer Sale	20% off all summer items	30	2025-06-14	2025-07-15	\N	\N	percentage	f	1	all
+4	Summer Sale	20% off all summer items	40	2025-06-14	2025-07-15	\N	\N	percentage	f	1	all
+1	Summer Sale	20% off all summer items	20	2025-06-14	2025-07-15	\N	2025-06-11 18:30:16.060139	percentage	f	1	all
+5	Spooky Sale	20% off all hallowing items	20	2025-06-14	2025-07-15	2025-06-11 18:34:47.58427	2025-06-11 18:34:47.58427	percentage	f	1	all
+7	Spooky Sale Added Value	20% off all hallowing items + Free Wine	\N	2025-06-14	2025-07-15	2025-06-11 18:42:04.111194	2025-06-11 18:42:04.111194	added_value	f	1	all
+14	Regular	Test description	20	2025-06-15	2025-07-15	2025-06-13 19:16:41.736387	2025-06-13 19:16:41.736387	percentage	t	1	all
 \.
 
 
 --
--- TOC entry 4811 (class 0 OID 0)
--- Dependencies: 218
+-- TOC entry 4447 (class 0 OID 0)
+-- Dependencies: 216
 -- Name: promotions_promotion_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
@@ -168,7 +189,7 @@ SELECT pg_catalog.setval('public.promotions_promotion_id_seq', 14, true);
 
 
 --
--- TOC entry 4655 (class 2606 OID 25073)
+-- TOC entry 4293 (class 2606 OID 25098)
 -- Name: promotions promotions_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -177,7 +198,7 @@ ALTER TABLE ONLY public.promotions
 
 
 --
--- TOC entry 4656 (class 2620 OID 25074)
+-- TOC entry 4294 (class 2620 OID 25099)
 -- Name: promotions trigger_delete_test_promotions; Type: TRIGGER; Schema: public; Owner: postgres
 --
 
@@ -185,14 +206,14 @@ CREATE TRIGGER trigger_delete_test_promotions AFTER DELETE ON public.promotions 
 
 
 --
--- TOC entry 4657 (class 2620 OID 25075)
+-- TOC entry 4295 (class 2620 OID 25100)
 -- Name: promotions trigger_promotions_updated_at; Type: TRIGGER; Schema: public; Owner: postgres
 --
 
 CREATE TRIGGER trigger_promotions_updated_at BEFORE UPDATE ON public.promotions FOR EACH ROW EXECUTE FUNCTION public.update_updated_at_column();
 
 
--- Completed on 2025-06-15 22:29:11
+-- Completed on 2025-06-16 08:26:49 -05
 
 --
 -- PostgreSQL database dump complete
