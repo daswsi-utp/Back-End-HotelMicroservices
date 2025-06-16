cca85bf881b50a8738e80d40b67b12c5658b2afc
KenyiPlatanazo <riositalo2@gmail.com>
2025-06-16 14:29:37 -0500

    ADDED: RoomDTO to fetch data from rooms

---
+++ microservice-promotions/src/main/java/com/microservice_promotions/dto/RoomDTO.java
@@ -0,0 +1,14 @@
+package com.microservice_promotions.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class RoomDTO {
+    private Long roomId;
+    private String name;
+}
