bce2f7f9970297c090017a068ad5ef27a313b39e
italo <riositalo2@gmail.com>
2025-06-16 19:07:03 -0500

    ADDED Promotion request dto class to be used in the front end

--- microservice-promotions/src/main/java/com/microservice_promotions/dto/PromotionResponseDTO.java
+++ microservice-promotions/src/main/java/com/microservice_promotions/dto/PromotionResponseDTO.java
@@ -3,17 +3,21 @@ package com.microservice_promotions.dto;
 import com.microservice_promotions.entitites.Promotion;
 
 import java.sql.Date;
+import java.sql.Timestamp;
 import java.util.Set;
 
-public class PromotionRequestDTO {
+public class PromotionResponseDTO {
+    Long promotionId;
     String name;
     String description;
     Double discountValue;
     Date startDate;
     Date endDate;
+    Timestamp createdAt;
+    Timestamp updatedAt;
     Promotion.Type type;
     Boolean isActive;
     int minStay;
     Promotion.RoomApplicability roomApplicability;
-    Set<Long> roomIds;
+    Set<RoomDTO> rooms;
 }
