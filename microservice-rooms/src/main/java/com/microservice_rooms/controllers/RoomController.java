a8444628e2c971474b8b6336e5b56872651b96e9
Im3Im3 <manuelcode737@gmail.com>
2025-06-18 11:30:50 -0500

    Add Croos Origin

--- microservice-rooms/src/main/java/com/microservice_rooms/controllers/RoomController.java
+++ microservice-rooms/src/main/java/com/microservice_rooms/controllers/RoomController.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/rooms")
 public class RoomController {
