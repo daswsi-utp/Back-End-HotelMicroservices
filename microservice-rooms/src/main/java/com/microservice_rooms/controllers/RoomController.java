17bd170bb56b7abfeb9ad16bc52dc1d07be40ccf
KenyiPlatanazo <riositalo2@gmail.com>
2025-06-21 18:58:50 -0500

    TEST: Commit to be rolled after testing promotion connection with gateway

--- microservice-rooms/src/main/java/com/microservice_rooms/controllers/RoomController.java
+++ microservice-rooms/src/main/java/com/microservice_rooms/controllers/RoomController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
-@RequestMapping("/api/rooms")
+//@RequestMapping("/api/rooms")
+@RequestMapping
 public class RoomController {
 
     @Autowired
