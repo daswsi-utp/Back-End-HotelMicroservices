65a87a0c110853f1a256859e3026b83b068c54f2
KenyiPlatanazo <riositalo2@gmail.com>
2025-06-27 13:39:06 -0500

    FIXED: Typo in promotions feign client

--- microservice-promotions/src/main/java/com/microservice_promotions/client/RoomClient.java
+++ microservice-promotions/src/main/java/com/microservice_promotions/client/RoomClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "micro-rooms")
+@FeignClient(name = "microservice-rooms")
 // @FeignClient(name = "micro-rooms", url = "http://localhost:8090/api/rooms")
 public interface RoomClient {
    @GetMapping("/type/{id}")
