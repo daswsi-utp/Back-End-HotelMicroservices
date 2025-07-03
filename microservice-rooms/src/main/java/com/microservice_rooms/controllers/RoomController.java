ad0004c5314ed2be9aaefc824e5215ad4f271b5c
jackutp <u21318614@utp.edu.pe>
2025-07-03 01:01:41 -0500

    add cors

--- microservice-rooms/src/main/java/com/microservice_rooms/controllers/RoomController.java
+++ microservice-rooms/src/main/java/com/microservice_rooms/controllers/RoomController.java
@@ -17,7 +17,7 @@ import java.io.IOException;
 import java.util.*;
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("/api/rooms")
+//@RequestMapping("/api/rooms")
 public class RoomController {
 
     @Autowired
