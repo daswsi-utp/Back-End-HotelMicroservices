8d9c6b09509202922eb48c358559b4cd1f5ddb85
jackutp <u21318614@utp.edu.pe>
2025-07-03 00:50:43 -0500

    add cors

--- microservice-rooms/src/main/java/com/microservice_rooms/controllers/RoomTypeController.java
+++ microservice-rooms/src/main/java/com/microservice_rooms/controllers/RoomTypeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("/api/roomtype")
+//@RequestMapping("/api/roomtype")
 
 public class RoomTypeController {
 
