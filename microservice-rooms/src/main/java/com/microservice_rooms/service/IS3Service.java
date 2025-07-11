81166e3fc08848de70469566d94c3da18e87271a
italo <riositalo2@gmail.com>
2025-07-11 18:09:43 -0500

    ADDED: Basic AWS3 configuration, StorageConfig with beans file and base interface
    This is added so images don't get deleted when a microservices goes to sleep

---
+++ microservice-rooms/src/main/java/com/microservice_rooms/service/IS3Service.java
@@ -0,0 +1,16 @@
+package com.microservice_rooms.service;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.time.Duration;
+import java.util.List;
+
+public interface IS3Service {
+    String createBucket(String bucketName);
+    String checkIfBucketExists(String bucketName);
+    List<String> getAllBuckets();
+    Boolean uploadFile(String bucketName, String key, Path fileLocation);
+    void downloadFile(String bucket, String key) throws IOException;
+    String generatePreSignedUpUploadUrl(String bucketName, String key, Duration duration);
+    String generatePreSignedUpDownloadUrl(String bucketName, String key, Duration duration);
+}
