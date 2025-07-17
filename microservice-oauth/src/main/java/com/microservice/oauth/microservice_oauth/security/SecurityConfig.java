86cb2fe5fe462177a792fbc9a9453882a384b2c3
KenyiPlatanazo <riositalo2@gmail.com>
2025-07-17 04:24:50 -0500

    Please work I'm begging you

--- microservice-oauth/src/main/java/com/microservice/oauth/microservice_oauth/security/SecurityConfig.java
+++ microservice-oauth/src/main/java/com/microservice/oauth/microservice_oauth/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
@@ -52,6 +53,7 @@ public class SecurityConfig {
                                 "/login",
                                 "/actuator/**"
                         ).permitAll()
+                        .pathMatchers(HttpMethod.OPTIONS).permitAll()
                         .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
